4d4d3cff792167424dde83439876e214a1992b67
olbinout <testeotavio124@gmail.com>
2026-09-15 12:13:16 -0300

    Organização de código e criação do .md

--- Unidade1/Lista01_Exercicios/questao6_Matriz/Matriz.java
+++ Unidade1/Lista01_Exercicios/questao6_Matriz/Matriz.java
@@ -1,7 +1,6 @@
 package Unidade1.Lista01_Exercicios.questao6_Matriz;
 
 import java.util.Scanner;
-
 public class Matriz {
     private int[][] valores;
 
@@ -15,7 +14,7 @@ public class Matriz {
 
     public void imprimir(){
         for (int i = 0; i < 3; i++){
-            for(int j = 0; j < 3; j++){
+            for (int j = 0; j < 3; j++){
                 System.out.print(getValores()[i][j] + " ");
             }
             System.out.println();
@@ -24,20 +23,19 @@ public class Matriz {
 
     public static void main (String[] Args){
         Scanner meuScanner = new Scanner(System.in);
-        int[][] dados = new int[3][3]; 
-
+        int[][] dados = new int[3][3];
         for (int i = 0; i < 3; i++){
-            for (int j = 0; j < 3; j++){
-                System.out.printf("Linha %d Coluna %d: ", i , j);
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("Linha %d / Coluna %d: ", i, j);
                 dados[i][j] = meuScanner.nextInt();
             }
         }
 
+        meuScanner.close();
+
         Matriz minhaMatriz = new Matriz(dados);
 
         minhaMatriz.imprimir();
-
-        meuScanner.close();
     }
 
 }
